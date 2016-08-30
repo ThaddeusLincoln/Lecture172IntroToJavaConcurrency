61519efbbbe2c12b0e9e647bf0be659bc4afd502
ThaddeusLincoln <beny1419@yahoo.es>
2016-08-30 23:18:57 +0200

    We can not start a Thread subclass's instance more than once. It will cause a java.lang.IllegalThreadStateException.

--- Lect172IntroductionToConcurrency/src/com/turing/jdev/threads/Main.java
+++ Lect172IntroductionToConcurrency/src/com/turing/jdev/threads/Main.java
@@ -3,10 +3,19 @@ package com.turing.jdev.threads;
 public class Main {
 	
 	public static void main(String[] args){
+
+		// 1
 		System.out.println("Hello from the main thread");
 		
+		// 2
 		AnotherThread anotherThread = new AnotherThread();
 		anotherThread.start();
+		
+		// 3 java.lang.IllegalThreadStateException
+		System.out.println("Hello AGAIN from the main thread");
+		
+		// will cause 
+		//anotherThread.start();
 	}
 
 }
