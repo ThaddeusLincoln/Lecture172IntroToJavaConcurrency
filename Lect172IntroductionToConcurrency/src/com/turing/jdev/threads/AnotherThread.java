039be41ee61e23e9f54c3293e80782e1c591204d
ThaddeusLincoln <beny1419@yahoo.es>
2016-08-31 01:43:15 +0200

    Putting a thread to sleep.

--- Lect172IntroductionToConcurrency/src/com/turing/jdev/threads/AnotherThread.java
+++ Lect172IntroductionToConcurrency/src/com/turing/jdev/threads/AnotherThread.java
@@ -4,7 +4,18 @@ public class AnotherThread extends Thread {
 
 	@Override
 	public void run() {
+		
 		System.out.println("Hello from " + currentThread().getName());
+		
+		// playing with the thread
+		try{
+			Thread.sleep(3000);
+		}catch(InterruptedException e){
+			// this line will be executed if another thread wakes this one
+			System.out.println("Another therad woke me up");
+		}
+		
+		System.out.println("3 seconds are passed and " + currentThread().getName() + "is awake again");
 	}
 	
 	
